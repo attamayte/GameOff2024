3cdc34d200671e1e9da336387cbc2f8dbe40b25c
Alex Svetlyy <36406930+attamayte@users.noreply.github.com>
2024-11-02 21:42:03 +0100

    Biome and road resources + road generator v.0

---
+++ prototype/biome.gd
@@ -0,0 +1,4 @@
+class_name Biome extends Resource
+
+@export var repeated_chunks: Array[PackedScene]
+@export var unique_chunks: Array[PackedScene]
--- prototype/road_params.gd
+++ prototype/road_params.gd
@@ -1,17 +1,18 @@
 class_name RoadParams extends Resource
 
-enum Biome { FOREST, PLAINS, WASTELAND }
-
-@export_category("Params")
-@export var biome: Biome = Biome.FOREST
+@export var biome: Biome
 @export var length_min: int = 1
 @export var length_max: int = 1
 @export var poi_min: int = 0
 @export var poi_max: int = 0
 @export var unique_poi_chance: float = 0.0
-@export_category("Assets")
-@export var chunks: Array[PackedScene]
+
+func get_repeated_size() -> int: 
+	return biome.repeated_chunks.size()
+
+func get_repeated_chunks() -> Array[PackedScene]:
+	return biome.repeated_chunks
 
 func get_random_length() -> int:
 	var random_length: int = randi_range(length_min, length_max)
-	return mini(random_length, chunks.size())
+	return mini(random_length, get_repeated_size())
